05e2e3e0fa99b4ee5c7d5b8f218f29e1e5a96d4b
matt <matthew.motallebipour@cognibotics.com>
2023-06-19 17:13:16 +0200

    added the solution

--- directorofphotography1_test.py
+++ directorofphotography1_test.py
@@ -1,1 +1,9 @@
+import pytest
+import directorofphotography1 as dop
 
+def test_getArtisticPhotographCount(nr_cells, positions, min_dist, max_dist, nr_photographs):
+    assert dop.getArtisticPhotographCount(nr_cells, positions, min_dist, max_dist) == nr_photographs
+
+test_getArtisticPhotographCount(5, 'APABA', 1, 2, 1)
+test_getArtisticPhotographCount(5, 'APABA', 2, 3, 0)
+test_getArtisticPhotographCount(8, '.PBAAP.B', 1, 3, 3)
