78eae591abfdb93fef2682bfa94dfb59494603ea
matt <matthew.motallebipour@cognibotics.com>
2023-06-19 17:20:21 +0200

    added some comments

--- directorofphotography1.py
+++ directorofphotography1.py
@@ -1,13 +1,18 @@
-# 
+# get the positions of a particular letter in the string
 def get_indices(s, ch):
     return [i for i, ltr in enumerate(s) if ltr == ch]
 
+# check if the photographer (p), actor (a), and backdrop (b) are in the
+# correct order
 def is_between(p, a, b):
     return (p < a & a < b) | (b < a & a < p)
 
+# check if the interval is correct
 def in_interval(a, p, X, Y):
     return X <= abs(a - p) & abs(a - p) <= Y
 
+# check if the distances between the three (p-a-b) make
+# an artistic photograph
 def is_legitimate(p, a, b, X, Y):
     return in_interval(a, p, X, Y) & in_interval(b, a, X, Y) & is_between(p, a, b)
 
